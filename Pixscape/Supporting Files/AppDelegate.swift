e39ef2527b08f1da1db144cb44a7318dc6856440
Bilal Souti <bilal@scape.io>
2019-03-10 12:52:59 +0000

    - Remove Crashlytics

--- Pixscape/Supporting Files/AppDelegate.swift
+++ Pixscape/Supporting Files/AppDelegate.swift
@@ -27,8 +27,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        Fabric.with([Crashlytics.self])
-        
         scapeClient.scapeClientObserver = self
         scapeClient.start()
         
